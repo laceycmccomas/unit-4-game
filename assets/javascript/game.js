f44f2132cf52ba04d5e69af656f7e7b2114974e4
thelaceyedit <thelaceyedit@gmail.com>
2018-06-28 18:52:56 -0400

    final hw

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 	var random=Math.floor(Math.random()*101+19)
 	// Selects a random number to be shown at the start of the game
 
-	$('#randomNumber').text(Random);
-	
+	$('#randomNumber').text(random);
+
 	//
 	var num1= Math.floor(Math.random()*11+1)
 	var num2= Math.floor(Math.random()*11+1)
@@ -11,14 +11,14 @@ $(document).ready(function() {
 	var num4= Math.floor(Math.random()*11+1)
 	// Setting up random numbers for each jewel
 	// Random number has to be between 1 - 12
-	// 
-	var userTotal= 0; 
+	//
+	var userTotal= 0;
 	var wins= 0;
 	var losses = 0;
 	//  Decaring variables
   $('#numberWins').text(wins);
 	$('#numberLosses').text(losses);
-	
+
   //resets the game
   function reset(){
 		random=Math.floor(Math.random()*101+19);
@@ -35,11 +35,11 @@ $(document).ready(function() {
   //adds the wins to the userTotal
   	function yay(){
   		alert("You won!");
-			wins++; 
+			wins++;
 		$('#numberWins').text(wins);
 			reset();
 	};
-	
+
 
   //addes the losses to the userTotal
   	function loser(){
@@ -48,33 +48,33 @@ $(document).ready(function() {
 		$('#numberLosses').text(losses);
 			reset()
 	};
-	
+
 
   //sets up click for jewels
 	$('#crystal').on ('click', function(){
 	  userTotal = userTotal + num1;
 	  console.log("userTotal=" + userTotal);
-	  $('#finalTotal').text(userTotal); 
+	  $('#finalTotal').text(userTotal);
 			//sets win/lose condition
 		  if (userTotal == random){
 			yay();
 		  }
 		  else if ( userTotal > random){
 			loser();
-		  }   
+		  }
 	});
-	  
+
 	$('#crystalOne').on ('click', function(){
 	  userTotal = userTotal + num2;
 	  console.log("userTotal=" + userTotal);
-	  $('#finalTotal').text(userTotal); 
+	  $('#finalTotal').text(userTotal);
 		  if (userTotal == random){
 			yay();
 		  }
 		  else if ( userTotal > random){
 			loser();
-		  } 
-	});  
+		  }
+	});
 	$('#crystalTwo').on ('click', function(){
 	  userTotal = userTotal + num3;
 	  console.log("userTotal=" + userTotal);
@@ -85,13 +85,13 @@ $(document).ready(function() {
 		  }
 		  else if ( userTotal > random){
 			loser();
-		  } 
-	});  
+		  }
+	});
 	$('#crystalThree').on ('click', function(){
 	  userTotal = userTotal + num4;
 	  console.log("userTotal=" + userTotal);
-	  $('#finalTotal').text(userTotal); 
-		
+	  $('#finalTotal').text(userTotal);
+
 			if (userTotal == random){
 			yay();
 		  }
@@ -99,3 +99,5 @@ $(document).ready(function() {
 			loser();
 			}
 		});
+
+	});
